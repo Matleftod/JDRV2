ce305211974c7840e9cb16eb5d5433b8f8305c51
Matéo Girollet Dit Androt <mateo.girollet33@gmail.com>
2023-03-05 00:48:56 +0100

    gold V2

--- src/components/Gold.jsx
+++ src/components/Gold.jsx
@@ -1,49 +1,89 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { TextField } from '@material-ui/core';
 import CurrencyBitcoinIcon from '@mui/icons-material/CurrencyBitcoin';
 import { db } from '../firebase';
 
-const Gold = ({ gold, name }) => {
-  const [editing, setEditing] = useState(false);
-  const [newGold, setNewGold] = useState(gold);
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    color: 'gold',
+  },
+  icon: {
+    marginRight: '8px',
+  },
+  input: {
+    width: '60px',
+    marginLeft: '8px',
+    marginRight: '8px',
+    '& input': {
+      textAlign: 'right',
+    },
+  },
+}));
 
-  const handleEditClick = () => {
+function Gold({ gold, name }) {
+  const classes = useStyles();
+  const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState(gold);
+
+  const handleInputChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleFocus = (event) => {
+    event.target.select();
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSave();
+    }
+  };
+
+  const handleSave = () => {
+    setEditing(false);
+    db.collection('personnage')
+      .where('name', '==', name)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          db.collection('personnage').doc(doc.id).update({
+            gold: parseInt(value, 10),
+          });
+        });
+      });
+  };
+
+  const handleClick = () => {
     setEditing(true);
   };
 
-  const handleCancelClick = () => {
-    setEditing(false);
-    setNewGold(gold);
-  };
-
-  const handleSaveClick = async () => {
-    await db.collection("personnage").where("name", "==", name).get().then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        db.collection("personnage").doc(doc.id).update({ gold: newGold });
-      });
-    });
-    setEditing(false);
-  };
-
-  const handleGoldChange = (e) => {
-    setNewGold(e.target.value);
-  };
-
-  if (editing) {
-    return (
-      <div>
-        <input type="number" value={newGold} onChange={handleGoldChange} />
-        <button onClick={handleSaveClick}>Save</button>
-        <button onClick={handleCancelClick}>Cancel</button>
-      </div>
-    );
-  }
-
   return (
-    <div onClick={handleEditClick}>
-      <CurrencyBitcoinIcon />
-      <span>{gold}</span>
+    <div className={classes.root}>
+      <CurrencyBitcoinIcon className={classes.icon} />
+      {editing ? (
+        <TextField
+          className={classes.input}
+          type="number"
+          value={value}
+          onChange={handleInputChange}
+          onFocus={handleFocus}
+          onKeyPress={handleKeyPress}
+          onBlur={handleSave}
+          autoFocus
+          inputProps={{
+            min: 0,
+          }}
+        />
+      ) : (
+        <span style={{ fontSize: '24px' }} onClick={handleClick}>
+          {gold}
+        </span>
+      )}
     </div>
   );
-};
+}
 
 export default Gold;
