aeb07b49f3ed8688bde3a49fb1b3140cebc2d378
Matéo Girollet Dit Androt <mateo.girollet33@gmail.com>
2023-03-05 15:36:11 +0100

    betterHud + health buton

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Typography } from '@material-ui/core';
+import { Container, IconButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import HealthBar from './HealthBar';
 import { db } from '../firebase';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,11 +12,34 @@ const useStyles = makeStyles((theme) => ({
   header: {
     marginBottom: theme.spacing(2),
   },
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginBottom: theme.spacing(2),
+    position: 'absolute',
+    top: '10px',
+    right: '10px',
+    background: 'rgba(0, 0, 0, 0.5)',
+    borderRadius: '10px',
+    "&:hover": {
+      background: "transparent"
+    }
+  },
+  healthContainer:{
+    background: 'rgba(0, 0, 0, 0.5)',
+    borderRadius: '10px',
+    padding: '20px',
+  },
+  heartIcon: {
+    color: 'red',
+  },
 }));
 
 function Home() {
   const classes = useStyles();
   const [characters, setCharacters] = useState([]);
+  const [showHealthBars, setShowHealthBars] = useState(true);
 
   useEffect(() => {
     // Attach a realtime listener to monitor changes to the "personnage" collection
@@ -28,11 +52,24 @@ function Home() {
     return () => unsubscribe();
   }, []);
 
+  const toggleHealthBars = () => {
+    setShowHealthBars(!showHealthBars);
+  }
+
   return (
     <Container maxWidth="sm" className={classes.root}>
-      {characters.map((character) => (
-        <HealthBar key={character.id} name={character.name} currentHealth={character.pv} maxHealth={character.max_pv} gold={character.gold}/>
-      ))}
+      <div className={classes.buttonContainer}>
+        <IconButton onClick={toggleHealthBars}>
+          <FavoriteIcon className={classes.heartIcon} />
+        </IconButton>
+      </div>
+      {showHealthBars && (
+        <div className={classes.healthContainer}>
+          {characters.map((character) => (
+            <HealthBar key={character.id} name={character.name} currentHealth={character.pv} maxHealth={character.max_pv} gold={character.gold}/>
+          ))}
+        </div>
+      )}
     </Container>
   );
 }
