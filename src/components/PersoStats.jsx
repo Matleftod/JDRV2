46539556a2897f6032c173d41ae0d2ffec461013
Matleftod <97225801+Matleftod@users.noreply.github.com>
2023-03-22 18:53:59 +0100

    Add all stats + style

--- src/components/PersoStats.jsx
+++ src/components/PersoStats.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Typography, IconButton } from '@material-ui/core';
+import { Container, Typography, IconButton, Grid, Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import CloseIcon from '@material-ui/icons/Close';
@@ -27,6 +27,45 @@ const useStyles = makeStyles((theme) => ({
     background: 'rgba(0, 0, 0, 0.85)',
     borderRadius: '10px',
     padding: theme.spacing(2),
+    textAlign: 'left',
+  },
+  gridItem: {
+    padding: theme.spacing(1),
+    margin: theme.spacing(1),
+    borderRadius: '10px',
+  },
+  name: {
+    marginBottom: '20px', 
+    fontSize: '1.5rem',
+    fontWeight: 'bold',
+  },
+  statsAtk: {
+    border: 'solid 1px red',
+    borderRadius: '10px',
+    padding: theme.spacing(2),
+  },
+  statsCerv: {
+    border: 'solid 1px cyan',
+    borderRadius: '10px',
+    padding: theme.spacing(2),
+  },
+  statsDext: {
+    border: 'solid 1px green',
+    borderRadius: '10px',
+    padding: theme.spacing(2),
+  },
+  statsMental: {
+    border: 'solid 1px violet',
+    borderRadius: '10px',
+    padding: theme.spacing(2),
+  },
+  statsHaki: {
+    border: 'solid 1px blue',
+    borderRadius: '10px',
+    padding: theme.spacing(2),
+  },
+  category: {
+    fontWeight: 'bold',
   },
 }));
 
@@ -74,14 +113,53 @@ return (
         </div>
         {characterStats && (
         <div className={classes.statsContainer}>
-            <Typography variant="h6">{characterStats.name}</Typography>
-            <Typography>Catégorie Combat : </Typography>
-            <Typography>Arme Légère : {characterStats.arm_legere}</Typography>
-            <Typography>Arme Lourde : {characterStats.arm_lourde}</Typography>
-            <Typography>Mains Nues : {characterStats.mains_n}</Typography>
-            <Typography>Arme à distance : {characterStats.arm_dist}</Typography>
-            <Typography>Style Spécial : {characterStats.style_spe}</Typography>
-            {/* Add other statistics as needed */}
+            <Typography variant="h6" className={classes.name}>
+                {characterStats.name}
+            </Typography>
+            <Grid container spacing={4}>
+                <Grid item xs={12} sm={6}>
+                    <Box className={`${classes.gridItem} ${classes.statsAtk}`}>
+                        <Typography className={classes.category}>Catégorie Combat : </Typography>
+                        <Typography>Arme Légère : {characterStats.arm_legere}</Typography>
+                        <Typography>Arme Lourde : {characterStats.arm_lourde}</Typography>
+                        <Typography>Mains Nues : {characterStats.mains_n}</Typography>
+                        <Typography>Arme à distance : {characterStats.arm_dist}</Typography>
+                        <Typography>Style Spécial : {characterStats.style_spe}</Typography>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <Box className={`${classes.gridItem} ${classes.statsCerv}`}>
+                        <Typography className={classes.category}>Catégorie Cerveau : </Typography>
+                        <Typography>Connaissance : {characterStats.connaissance}</Typography>
+                        <Typography>Intelligence : {characterStats.intel}</Typography>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <Box className={`${classes.gridItem} ${classes.statsDext}`}>
+                        <Typography className={classes.category}>Catégorie Dextérité : </Typography>
+                        <Typography>Agilité / Discrérité : {characterStats.agi}</Typography>
+                        <Typography>Perception : {characterStats.perception}</Typography>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <Box className={`${classes.gridItem} ${classes.statsMental}`}>
+                        <Typography className={classes.category}>Catégorie Mental : </Typography>
+                        <Typography>Intimidation : {characterStats.intimidation}</Typography>
+                        <Typography>Mensonge : {characterStats.mensonge}</Typography>
+                        <Typography>Négociation : {characterStats.negocia}</Typography>
+                        <Typography>Charisme : {characterStats.charisme}</Typography>
+                        <Typography>Volonté : {characterStats.volonte}</Typography>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <Box className={`${classes.gridItem} ${classes.statsHaki}`}>
+                        <Typography className={classes.category}>Catégorie Haki : </Typography>
+                        <Typography>Haki de l'Armement : {characterStats.haki_arm}</Typography>
+                        <Typography>Haki de l'observation : {characterStats.haki_obs}</Typography>
+                        <Typography>Haki des Rois : {characterStats.haki_roi}</Typography>
+                    </Box>
+                </Grid>
+            </Grid>
         </div>
         )}
     </Container>
